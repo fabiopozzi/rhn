cad941a3bc3caecf80c33df59195db83c6c0af88
fabio <fabio@antani.work>
2016-08-07 13:12:39 +0200

    show titles in ncurses page, one for each line

--- p2.rb
+++ p2.rb
@@ -1,12 +1,35 @@
 #!/usr/bin/env ruby
 require 'rubygems'
+require "ncursesw"
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open('/Users/fabio/Desktop/index.html'))
-#page = Nokogiri::HTML(open("https://news.ycombinator.com/"))
-titles = page.xpath("//td[@class='title']")
+begin
+  # initialize ncurses
+  Ncurses.initscr
+  Ncurses.cbreak						# provide unbuffered input
+	Ncurses.noecho						# turn off input echo
+	Ncurses.nonl							# turn off newline translation
+	Ncurses.stdscr.intrflush(false)	# turn off flush-on-interrupt
+	Ncurses.stdscr.keypad(true)			# turn on keypad mode
 
-titles.each do |t|
-	puts t.text.to_s.encode("ISO-8859-1")
+
+	page = Nokogiri::HTML(open('/Users/fabio/Desktop/index.html'))
+	#page = Nokogiri::HTML(open("https://news.ycombinator.com/"))
+	titles = page.xpath("//td[@class='title']")
+
+	row = 4
+	titles.each do |t|
+		#puts t.text.to_s.encode("ISO-8859-1")
+		Ncurses.stdscr.mvaddstr(row, 5, t.text.to_s.encode("ISO-8859-1"))
+		row += 1
+	end
+	Ncurses.refresh
+	Ncurses.stdscr.getch
+
+ensure
+	Ncurses.echo
+	Ncurses.nocbreak
+	Ncurses.nl
+	Ncurses.endwin
 end
