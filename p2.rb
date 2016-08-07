69e91fb154219be432ef04de078dbcf76b3fd7fd
fabio <fabio@antani.work>
2016-08-07 10:15:50 +0200

    extract page titles using xpath

--- p2.rb
+++ p2.rb
@@ -3,6 +3,10 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-#page = Nokogiri::HTML(open('/Users/fabio/Desktop/index.html'))
-page = Nokogiri::HTML(open("https://news.ycombinator.com/"))
-puts page.class
+page = Nokogiri::HTML(open('/Users/fabio/Desktop/index.html'))
+#page = Nokogiri::HTML(open("https://news.ycombinator.com/"))
+titles = page.xpath("//td[@class='title']")
+
+titles.each do |t|
+	puts t.text
+end
